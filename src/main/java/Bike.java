99e723de1c24fd393597819ca7d7bca24063d266
jenniefang <jennie.fang@mail.utoronto.ca>
2021-09-27 11:46:38 -0400

    A Bike Class

---
+++ src/main/java/Bike.java
@@ -0,0 +1,36 @@
+
+public class Bike implements Drivable, Tradable{
+    private int maxSpeed;
+
+    /**
+     * A bike that is tradable and drivable.
+     */
+    public Bike() { this.maxSpeed = 5 }
+
+    /**
+     * Speed up bike.
+     */
+    @Override
+    public void upgradeSpeed() { this.maxSpeed = this.maxSpeed + 2; }
+
+    /**
+     * Slow down bike.
+     */
+    @Override
+    public void downgradeSpeed() { this.maxSpeed = this.maxSpeed - 2; }
+
+    /**
+     * Get maximum speed of Bike.
+     *
+     * @return speed of Bike
+     */
+    @Override
+    public int getMaxSpeed() { return this.maxSpeed; }
+
+    /**
+     * Get price of Bike.
+     */
+    @Override
+    public int getPrice() { return 15; }
+    }
+}
