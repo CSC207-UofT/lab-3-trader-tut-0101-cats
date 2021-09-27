86ed35da1d3387253b7e3581f2dc99a971faebbc
Caules Ge <74894206+CaulesGe@users.noreply.github.com>
2021-09-27 11:40:57 -0400

    Add files via upload

---
+++ src/test/java/CatTest.java
@@ -0,0 +1,36 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CatTest {
+    Cat c;
+
+    @Before
+    public void setUp() throws Exception {
+        c = new Cat();
+    }
+
+    @Test(timeout = 50)
+    public void TestSound() {
+        assertEquals("Nya", c.sound());
+    }
+
+
+    @Test(timeout = 50)
+    public void TestUpgradeSpeed() {
+        c.upgradeSpeed();
+        assertEquals(2, c.getMaxSpeed());
+    }
+
+    @Test(timeout = 50)
+    public void TestDowngradeSpeed() {
+        c.downgradeSpeed();
+        assertEquals(0, c.getMaxSpeed());
+    }
+
+    @Test(timeout = 50)
+    public void TestGetPrice() {
+        assertEquals(5, c.getPrice());
+    }
+}
