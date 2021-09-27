437e8cb9964edd688e6f4db276f990a8348947c1
IILIQUIDII <ericfeng8808@gmail.com>
2021-09-27 11:36:12 -0400

    test for smolSharky

---
+++ src/test/java/smolSharkyTest.java
@@ -0,0 +1,30 @@
+/*
+ * This file contains sample JUnit test cases for Horse.java
+ * You should NOT modify this file!
+ */
+
+import org.junit.*;
+
+import static org.junit.Assert.*;
+
+
+public class smolSharkyTest {
+    smolSharky sharky;
+
+    @Before
+    public void setUp() throws Exception {
+        sharky = new smolSharky();
+    }
+
+    @Test(timeout = 50)
+    public void TestSound() {
+        assertEquals("A!~~~", sharky.sound());
+    }
+
+
+    @Test(timeout = 50)
+    public void TestGetPrice() {
+        assertEquals(9001, sharky.getPrice());
+    }
+
+}
